ceb527045ece8ac1475803d7a5b14ac9b4fdefd9
Emmanuel Onyeyaforo <rahzy24@gmail.com>
2022-08-30 18:00:47 +0100

    Dev - board functionality

--- src/components/SideBar/BoardNamesTag.tsx
+++ src/components/SideBar/BoardNamesTag.tsx
@@ -19,8 +19,6 @@ const BoardNamesTag = () => {
     // const boardNameTagL = boardsData.map((val, i: number) => i)
     // let selectedStyle = boardIndexData === boardNameTagL
 
-    console.log(currentBoard)
-
     
 
 

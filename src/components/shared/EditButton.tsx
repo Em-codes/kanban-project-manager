a9e582f21e6b47ecdea83d1ec81b5079ba9889d5
Emmanuel Onyeyaforo <rahzy24@gmail.com>
2022-09-27 22:54:48 +0100

    Dev - Edit task functionality

--- src/components/shared/EditButton.tsx
+++ src/components/shared/EditButton.tsx
@@ -1,17 +1,22 @@
 import React, { useState, useRef } from 'react'
 import Image from 'next/image';
 import Modal from '@components/Modal';
-import DeleteBoardModal from '@components/Modal/DeleteBoardModal';
+import DeleteWarningModal from '@components/Modal/DeleteWarningModal';
 import UpdateBoardModal from '@components/Modal/UpdateBoardModal';
+import UpdateTaskModal from '@components/Modal/UpdateTaskModal';
 import { useOnClickOutside } from 'usehooks-ts'
+import { Task } from '@src/types';
+import { taskCompleted } from '@reduxjs/toolkit/dist/listenerMiddleware/exceptions';
 
 
 interface editButtonProps {
-  className: string,
+  task: Task
+  className: string
   currentBoard: string
+  type: string
 }
 
-const EditButton = ({ className, currentBoard }: editButtonProps) => {
+const EditButton = ({ type, task, className, currentBoard }: editButtonProps) => {
   const [showMenu, setShowMenu] = useState<boolean>(false);
   const [showUpdateBoardModal, setShowUpdateBoardModal] = useState<boolean>(false);
   const [showDeleteBoardModal, setShowDeleteBoardModal] = useState<boolean>(false);
@@ -26,29 +31,56 @@ const EditButton = ({ className, currentBoard }: editButtonProps) => {
       <button className="h-8 w-8" onClick={() => setShowMenu(!showMenu)}>
         <Image src="/assets/icon-vertical-ellipsis.svg" alt="vertical ellipsis" height={16} width={4} />
       </button>
-     {showMenu && 
-     <div ref={menuRef} className={`${className} flex flex-col items-start space-y-4 absolute body-lg rounded-lg p-4 w-48 shadow-main capitalize bg-white dark:bg-veryDarkGrey`}
-      >
-        <>
-          <button
-            className="text-mediumGrey text-[13px]"
-            onClick={() => setShowUpdateBoardModal(!showUpdateBoardModal)}
+      {showMenu &&
+    
+        (type === "Board" ? 
+          <div ref={menuRef} className={`${className} flex flex-col items-start space-y-4 absolute body-lg rounded-lg p-4 w-48 shadow-main capitalize bg-white dark:bg-veryDarkGrey`}
           >
-            Edit board
-          </button>
-          <Modal showModal={showUpdateBoardModal} setShowModal={() => setShowUpdateBoardModal(!showUpdateBoardModal)}>
-            <UpdateBoardModal setShowUpdateBoardModal={setShowUpdateBoardModal}/>
-          </Modal>
-          <button
-            className="text-mainRed  text-[13px]"
-            onClick={() => setShowDeleteBoardModal(true)}
-          >Delete board
-          </button>
-          <Modal showModal={showDeleteBoardModal} setShowModal={() => setShowDeleteBoardModal(!showDeleteBoardModal)}>
-            <DeleteBoardModal currentBoard={currentBoard} setShowDeleteBoardModal={setShowDeleteBoardModal} />
-          </Modal>
-        </>
-      </div> 
+            <>
+              <button
+                className="text-mediumGrey text-[13px]"
+                onClick={() => setShowUpdateBoardModal(!showUpdateBoardModal)}
+              >
+                Edit board
+              </button>
+              <Modal showModal={showUpdateBoardModal} setShowModal={() => setShowUpdateBoardModal(!showUpdateBoardModal)}>
+                <UpdateBoardModal setShowUpdateBoardModal={setShowUpdateBoardModal}/>
+              </Modal>
+              <button
+                className="text-mainRed  text-[13px]"
+                onClick={() => setShowDeleteBoardModal(true)}
+              >Delete board
+              </button>
+              <Modal showModal={showDeleteBoardModal} setShowModal={() => setShowDeleteBoardModal(!showDeleteBoardModal)}>
+                <DeleteWarningModal currentTask='' type='Board' currentBoard={currentBoard} setShowMenu={setShowMenu} setShowDeleteBoardModal={setShowDeleteBoardModal} />
+              </Modal>
+            </>
+          </div>  : 
+          // tasks CTA Modal
+          <div ref={menuRef} className={`${className} flex flex-col items-start space-y-4 absolute body-lg rounded-lg p-4 w-48 shadow-main capitalize bg-white dark:bg-veryDarkGrey`}
+          >
+            <>
+              <button
+                className="text-mediumGrey text-[13px]"
+                onClick={() => setShowUpdateBoardModal(!showUpdateBoardModal)}
+              >
+                Edit Task
+              </button>
+              <Modal showModal={showUpdateBoardModal} setShowModal={() => setShowUpdateBoardModal(!showUpdateBoardModal)}>
+                {/* <UpdateBoardModal setShowUpdateBoardModal={setShowUpdateBoardModal}/> */}
+                <UpdateTaskModal task={task}/>
+              </Modal>
+              <button
+                className="text-mainRed  text-[13px]"
+                onClick={() => setShowDeleteBoardModal(true)}
+              >Delete Task
+              </button>
+              <Modal showModal={showDeleteBoardModal} setShowModal={() => setShowDeleteBoardModal(!showDeleteBoardModal)}>
+                <DeleteWarningModal currentTask={task.title} type='' currentBoard={currentBoard} setShowMenu={setShowMenu} setShowDeleteBoardModal={setShowDeleteBoardModal} />
+              </Modal>
+            </>
+          </div>
+        )
       }
     </div>
 
